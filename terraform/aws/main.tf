78a3b9cf94165396f94c9597225937d3c3bfd412
H. Furkan Vural <hfurkanvural@gmail.com>
2020-11-19 00:51:27 +0300

    simple vpc & subnet resource added.

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,12 +1,27 @@
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
-resource "aws_instance" "demo"{
-    ami             = "ami-0502e817a62226e03"
-    instance_type   =  "t2.micro"
+resource "aws_instance" "demo" {
+  ami           = "ami-0502e817a62226e03"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "ubuntu-demo"
-    }
+  tags = {
+    Name = "ubuntu-demo"
+  }
+}
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "demo"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "demo-subnet"
+  }
 }
